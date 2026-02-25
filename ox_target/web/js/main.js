f6dcbc84477201049121817f16748bf63b949e85
takenncs <116774376+Takennncs@users.noreply.github.com>
2026-02-26 00:04:07 +0200

    Add files via upload

---
+++ ox_target/web/js/main.js
@@ -0,0 +1,48 @@
+import { createOptions } from "./createOptions.js";
+
+const optionsWrapper = document.getElementById("options-wrapper");
+const body = document.body;
+const eye = document.getElementById("eyeSvg"); // peab olema HTML-is olemas
+
+// Et veenduda, et element on DOMis
+if (!eye) {
+  console.warn("Eye element with id 'eyeSvg' not found!");
+}
+
+window.addEventListener("message", (event) => {
+  optionsWrapper.innerHTML = "";
+
+  switch (event.data.event) {
+    case "visible": {
+      body.style.visibility = event.data.state ? "visible" : "hidden";
+      if (eye) eye.classList.remove("eye-hover");
+      break;
+    }
+
+    case "leftTarget": {
+      if (eye) eye.classList.remove("eye-hover");
+      break;
+    }
+
+    case "setTarget": {
+      if (eye) eye.classList.add("eye-hover");
+
+      if (event.data.options) {
+        for (const type in event.data.options) {
+          event.data.options[type].forEach((data, id) => {
+            createOptions(type, data, id + 1);
+          });
+        }
+      }
+
+      if (event.data.zones) {
+        for (let i = 0; i < event.data.zones.length; i++) {
+          event.data.zones[i].forEach((data, id) => {
+            createOptions("zones", data, id + 1, i + 1);
+          });
+        }
+      }
+      break;
+    }
+  }
+});
